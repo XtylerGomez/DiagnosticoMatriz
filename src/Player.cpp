1c05a20d8bab571f9e78f86b22e7627c2e186aa9
Daniel Gomez <zap02173@zapopan.tecmm.edu.mx>
2023-02-15 16:43:13 -0600

    Cambios en CheckBorder y Add

--- src/Player.cpp
+++ src/Player.cpp
@@ -11,44 +11,49 @@ using namespace std;
 Matrix m;
 void Player::SetAxis(){ //inicio del juego, se establecen las coordenadas y se agregan.
     srand(time(NULL));
-    AxisX=rand()%10;
-    AxisY=rand()%10;
-    CheckBorder();
-    Add(AxisX, AxisY);
+    int x=rand()%10;
+    int y=rand()%10;
+    SetAxisX(x);
+    SetAxisY(y);
+    CheckBorder(x, y);
+    m.ShowMatrix(x, y);
 }
 
 void Player::Move(char key){//mover el jugador
     system("cls"); //como es consola ps tiene que estar limpia esta verdad ?
+
+    int x=GetAxisX();
+    int y=GetAxisY();
     switch(key){
         
         case 'w':   //Arriba
-        CheckBorder();
-        AxisX=AxisX-1;
-        Add(AxisX, AxisY);
+        x=x-1;
+        CheckBorder(x, y);
+
         break;
 
         case 'a':   //Izquierda
-        CheckBorder();
-        AxisY=AxisY-1;
-        Add(AxisX, AxisY);
+        y=y-1;
+        CheckBorder(x, y);
+
         break;
 
         case 's':   //Abajo
-        CheckBorder();
-        AxisX=AxisX+1;
-        Add(AxisX, AxisY);
+        x=x+1;
+        CheckBorder(x, y);
+
         break;
 
         case 'd':   //Derecha
-        CheckBorder();
-        AxisY=AxisY+1;
-        Add(AxisX, AxisY);
+        y=y+1;
+        CheckBorder(x, y);
+
         break;
 
         default:
         break;
     }
-    
+    m.ShowMatrix(GetAxisX(), GetAxisY());
 }
 
 /*BUGGEADO
@@ -59,23 +64,34 @@ void Player::Move(char key){//mover el jugador
     >con lo anterior, cambiar el tipo de funcion de void a Int, y retornar el Axis nuevo.
     >jugar con los valores del s y del 0 (valores de los limites)
 */
-void Player::CheckBorder(){//Checar la pocision para corregirla y dar la ilusion de matriz circular
+void Player::CheckBorder(int _x, int _y){//Checar la pocision para corregirla y dar la ilusion de matriz circular
 
     int s=(m.getSize()-1);
-    
-    if(AxisX==0){ //Limite Arriba
-        AxisX=s;
+    int x=0;
+    int y=0;
+    for(int i=-1;i<=1;i++){
+        for(int j=-1;j<=1;j++){
+            x=_x+i;
+            y=_y+j;
+            if(x=0){
+                x=s;
+            }
+            if(x=s){
+                x=0;
+            }
+            if(y=0){
+                y=s;
+            }
+            if(y=s){
+                y=0;
+            }
+            Add(x,y);
+            
+        }
     }
-    if(AxisX==s){ //Limite Abajo
-        AxisX=0;
-    }
-    if(AxisY==0){ //Limite Izquierda
-        AxisY=s;
-    }
-    if(AxisY==s){ //Limite Derecha
-        AxisY=0;
-    }
-
+    SetAxisX(_x);
+    SetAxisY(_y);
+}
 /*BUGGEADO
     Cuando se acerca a un borde no se traslada por el otro lado, sigue el orden de la matriz, ademas olvidé 
     el requerimiento de que no tiene que pasar de 9, haciendo que la matriz se desacomode.
@@ -85,24 +101,9 @@ void Player::CheckBorder(){//Checar la pocision para corregirla y dar la ilusion
     >aplicar la funcion de CheckBorder tambien a la cruz y a las esquinas
     >Combinar la funcion de Add y ChechBorder ?? 
 */
-}
-void Player::Add(int AxisX, int AxisY){//Sumas de los valores del jugador y alrededores
+
+void Player::Add(int _x, int _y){//Sumas de los valores del jugador y alrededores
     //Suma de la pocision del jugador
-    m.Matriz[AxisX][AxisY]++;
-
-    //la Cruz
-    m.Matriz[AxisX-1][AxisY]++;
-    m.Matriz[AxisX+1][AxisY]++;
-    m.Matriz[AxisX][AxisY-1]++;
-    m.Matriz[AxisX][AxisY+1]++;
-
-    //las Esquinas
-    m.Matriz[AxisX-1][AxisY-1]++;
-    m.Matriz[AxisX-1][AxisY+1]++;
-    m.Matriz[AxisX+1][AxisY-1]++;
-    m.Matriz[AxisX+1][AxisY+1]++;
-
-    //reiniciar el show de la matriz
-    m.ShowMatrix(AxisX, AxisY);
-    
+    m.Matriz[_x][_y]++;
+   
 }
--- src/Player.hpp
+++ src/Player.hpp
@@ -4,15 +4,22 @@
 
     class Player
     {
-    
-    public: 
+        
+    private:
         int AxisX; //Coordenadas
         int AxisY;
+
+    public: 
+        
         
         void SetAxis();
-        void CheckBorder();
+        void CheckBorder(int _x, int _y);
         void Move(char key);
         void Add(int _x, int _y);
+
+        void SetAxisX(int _x){AxisX=_x;}
+        void SetAxisY(int _y){AxisY=_y;}
+        
         int GetAxisX(){return AxisX;}//Getters. Tengo entendido que esto deberia hacerlo cuando los
         int GetAxisY(){return AxisY;}//parametros sean Privados, pero los voy aplicando de una
 
